3a7d630ec01d6034bf9c5c8ba94836a437304095
navid <n.ahmadi.livani@gmail.com>
2024-05-26 17:19:25 +0330

    single.post

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 
 # Create your views here.
 def blog_view(requst):
-    return render(requst, "blog/blog.html")
+    posts = Post.objects.filter(status=1)
+    context = {"posts": posts}
+    return render(requst, "blog/blog.html", context)
 
 
-def single_view(requst):
-    return render(requst, "blog/single-blog.html")
+def single_view(requst, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {"post": post}
+    return render(requst, "blog/single-blog.html", context)
